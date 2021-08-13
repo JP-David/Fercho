90edf6f975050478e6850fbc763e3ecc5c95bf39
JP-David <71202257+JP-David@users.noreply.github.com>
2021-08-13 16:13:11 -0500

    Create Excercise.py

---
+++ Excercise.py
@@ -0,0 +1,11 @@
+variable1= int(input('Dame un número: '))
+variable2= int(input("Dame un número: "))
+
+suma = variable1 + variable2 
+resta = variable1 - variable2
+multiplicación = variable1 * variable2
+
+      
+print("suma " + str(suma))
+print("resta  " + str(resta))
+print("multiplicación " + str(multiplicación))
